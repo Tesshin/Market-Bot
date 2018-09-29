ea594853e6ed8e2be9484947d51f9734cf85db5c
Tesshin <jumpy12359@gmail.com>
2018-09-29 22:49:59 +1000

    Check if TTL index exists

--- market-js.js
+++ market-js.js
@@ -1,5 +1,6 @@
 if (Number(process.version.slice(1).split(".")[0]) < 8) throw new Error("Node 8.0.0 or higher is required. Update Node on your system.");
 
+const MongoClient = require("mongodb").MongoClient;
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
@@ -29,8 +30,19 @@ fs.readdir("./commands/", (err, files) => {
     if (!file.endsWith(".js")) return;
     const props = require(`./commands/${file}`);
     const commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
+    console.log(`Attempting to load command: ${commandName}`);
     client.commands.set(commandName, props);
+  });
+  console.log("All commands have been loaded!");
+});
+
+MongoClient.connect(config.mongodb_uri, { useNewUrlParser: true }, function(err, db) {
+  const dbo = db.db("cs_pound");
+  dbo.collection("test").indexExists("createdAt_1", function(err, result) {
+    if (!result) {
+      dbo.collection("test").createIndex({ "createdAt": 1 }, { expireAfterSeconds: 0 });
+    }
+    db.close();
   });
 });
 
