f874cba1c6f18e65e836dc8af1e5c11b4b3e80c9
Tesshin <jumpy12359@gmail.com>
2018-09-29 22:48:33 +1000

    Formatting

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = async client => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity("By Peko#7955", { type: "PLAYING" })
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : "none"}`))
+    .then(presence => console.log(`Activity set to "${presence.game ? presence.game.name : "none"}"`))
     .catch(console.error);
   client.user.setStatus("online");
 };
